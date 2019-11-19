8c9adcefd56b6c8e742ea775389292824434cb80
Josue <josue.rosario@solvex.com.do>
2019-11-19 14:49:28 -0400

    itinerario start

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
+// $(window).scroll(function () {
+//     var scroll = $(window).scrollTop();
 
-    if (scroll >= 200) {
-        $(".transp-nav").addClass("new-nav");
-    } else {
-        $(".transp-nav").removeClass("new-nav");
-    }
+//     if (scroll >= 200) {
+//         $(".transp-nav").addClass("new-nav");
+//     } else {
+//         $(".transp-nav").removeClass("new-nav");
+//     }
+// });
+
+
+
+
+$(document).ready(function () {
+    $('.indicator').click(function () {
+        $(this).toggleClass('active-indicator');
+        $(this).siblings().removeClass('active-indicator');
+    });
 });
+
+
 
