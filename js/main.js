b43d22aa7db532a13bfdda9d3cfa77db61d945b8
Josue <josue.rosario@solvex.com.do>
2019-12-09 11:13:46 -0400

    texto-dentro del carousel

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
-$(window).scroll(function () {
-    var scroll = $(window).scrollTop();
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
 
-    if (scroll >= 200) {
-        $(".navbar").addClass("new-nav");
-    } else {
-        $(".navbar").removeClass("new-nav");
-    }
-
-
-
+  if (scroll >= 200) {
+    $(".navbar").addClass("new-nav");
+  } else {
+    $(".navbar").removeClass("new-nav");
+  }
 });
 
+$(document).ready(function() {
+  $(".indicator").click(function() {
+    $(this).toggleClass("active-indicator");
+    $(this)
+      .siblings()
+      .removeClass("active-indicator");
+  });
 
-
-
-
-$(document).ready(function () {
-    $('.indicator').click(function () {
-        $(this).toggleClass('active-indicator');
-        $(this).siblings().removeClass('active-indicator');
-
-    });
+  $(".carousel").carousel({
+    interval: 10000
+  });
 });
